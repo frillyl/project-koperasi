804b999973e4584c91cc26ab85c2fd237e646ee5
Frilly Lakumani <frillylakumani.1905@gmail.com>
2024-07-06 22:04:37 +0700

    Update favicon

--- app/Views/pengurus/layout/v_head.php
+++ app/Views/pengurus/layout/v_head.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?> | <?= $sub ?></title>
+    <link rel="shortcut icon" href="<?= base_url() ?>/public/assets/favicon_io/favicon.ico" type="image/x-icon">
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
